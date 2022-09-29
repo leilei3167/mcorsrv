1f0a2c94b5868d484968b3dd138331310e6b75da
leilei <imleilei123@gmail.com>
2022-09-29 23:17:01 +0800

    feat: 前端oss文件直传实现

---
+++ mall/mxshop_api/oss-web/initialize/config.go
@@ -0,0 +1,82 @@
+package initialize
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/nacos-group/nacos-sdk-go/clients"
+	"github.com/nacos-group/nacos-sdk-go/common/constant"
+	"github.com/nacos-group/nacos-sdk-go/vo"
+	"github.com/spf13/viper"
+	"go.uber.org/zap"
+
+	"mxshop_api/oss-web/global"
+)
+
+func GetEnvInfo(env string) bool {
+	viper.AutomaticEnv()
+	return viper.GetBool(env)
+	// 刚才设置的环境变量 想要生效 我们必须得重启goland
+}
+
+func InitConfig() {
+	release := GetEnvInfo("MXSHOP_DEBUG")
+	configFIlePrefix := "config"
+	configFileName := fmt.Sprintf("%s-pro.yaml", configFIlePrefix)
+	if !release {
+		configFileName = fmt.Sprintf("%s-debug.yaml", configFIlePrefix)
+	}
+	v := viper.New()
+	v.SetConfigFile(configFileName)
+
+	if err := v.ReadInConfig(); err != nil {
+		panic(err)
+	}
+	// 这个对象应该设置为全局变量
+
+	if err := v.Unmarshal(&global.NacosConfig); err != nil {
+		panic(err)
+	}
+	zap.S().Infof("配置信息: %v", global.NacosConfig)
+
+	// 从nacos中读取配置信息
+	sc := []constant.ServerConfig{
+		{
+			IpAddr: global.NacosConfig.Host,
+			Port:   global.NacosConfig.Port,
+		},
+	}
+
+	cc := constant.ClientConfig{
+		NamespaceId:         global.NacosConfig.Namespace, // 如果需要支持多namespace，我们可以场景多个client,它们有不同的NamespaceId
+		TimeoutMs:           5000,
+		NotLoadCacheAtStart: true,
+		LogDir:              "tmp/nacos/log",
+		CacheDir:            "tmp/nacos/cache",
+
+		LogLevel: "debug",
+	}
+
+	configClient, err := clients.CreateConfigClient(map[string]interface{}{
+		"serverConfigs": sc,
+		"clientConfig":  cc,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	content, err := configClient.GetConfig(vo.ConfigParam{
+		DataId: global.NacosConfig.DataId,
+		Group:  global.NacosConfig.Group,
+	})
+	if err != nil {
+		panic(err)
+	}
+	// fmt.Println(content) //字符串 - yaml
+	// 想要将一个json字符串转换成struct，需要去设置这个struct的tag
+	err = json.Unmarshal([]byte(content), &global.ServerConfig)
+	if err != nil {
+		zap.S().Fatalf("读取nacos配置失败： %s", err.Error())
+	}
+	fmt.Println(&global.ServerConfig)
+}
